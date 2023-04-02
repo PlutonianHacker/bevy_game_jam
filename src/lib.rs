bf7d180232c86685ab57e041b66c24ed9a240399
Luke VanWingerden <lukeeverett16@gmail.com>
2023-04-01 20:50:17 -0400

    Cleanup repo

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,3 @@
-mod actions;
-mod audio;
-mod loading;
-mod menu;
-mod player;
-
-use crate::actions::ActionsPlugin;
-use crate::audio::InternalAudioPlugin;
-use crate::loading::LoadingPlugin;
-use crate::menu::MenuPlugin;
-use crate::player::PlayerPlugin;
-
 use bevy::app::App;
 #[cfg(debug_assertions)]
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
@@ -25,21 +13,14 @@ enum GameState {
     Loading,
     // During this State the actual game logic is executed
     Playing,
-    // Here the menu is drawn and waiting for player interaction
-    Menu,
 }
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_state::<GameState>()
-            .add_plugin(LoadingPlugin)
-            .add_plugin(MenuPlugin)
-            .add_plugin(ActionsPlugin)
-            .add_plugin(InternalAudioPlugin)
-            .add_plugin(PlayerPlugin);
-
+        app.add_state::<GameState>();
+        
         #[cfg(debug_assertions)]
         {
             app.add_plugin(FrameTimeDiagnosticsPlugin::default())
