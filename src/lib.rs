8b4d8e0f1f1302f44478e0617504e378c1f1c5e7
PlutonianHacker <edmondjames16@gmail.com>
2023-04-01 21:44:23 -0400

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use bevy::app::App;
+use bevy::prelude::*;
+
 #[cfg(debug_assertions)]
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-use bevy::prelude::*;
 
 // This example game uses States to separate logic
 // See https://bevy-cheatbook.github.io/programming/states.html
