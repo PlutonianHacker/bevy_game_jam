bcf20a5dd6d382b4091813c1df00cefc5d4bedee
Luke VanWingerden <lukeeverett16@gmail.com>
2023-04-03 19:23:33 -0400

    Create loader

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,12 @@ use bevy::prelude::*;
 #[cfg(debug_assertions)]
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 
+mod level;
+mod loader;
+
+use level::LevelPlugin;
+use loader::LoadingPlugin;
+
 // This example game uses States to separate logic
 // See https://bevy-cheatbook.github.io/programming/states.html
 // Or https://github.com/bevyengine/bevy/blob/main/examples/ecs/state.rs
@@ -13,13 +19,16 @@ enum GameState {
     Loading,
     // During this State the actual game logic is executed
     Playing,
+    Transitioning,
 }
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_state::<GameState>();
+        app.add_state::<GameState>()
+            .add_plugin(LoadingPlugin)
+            .add_plugin(LevelPlugin);
         
         #[cfg(debug_assertions)]
         {
